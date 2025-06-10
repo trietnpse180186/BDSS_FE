04a0c6bca56b84381723beeec5f66b28b2c037f0
trietnpse180816 <trietnpse180186@fpt.edu.vn>
2025-06-10 07:54:40 +0700

    Update navbar.jsx

--- src/assets/navbar.jsx
+++ src/assets/navbar.jsx
@@ -1,19 +1,22 @@
 import { Link, useLocation } from "react-router-dom";
 import { Dropdown, Button, ButtonGroup } from "react-bootstrap";
 import { useEffect, useState } from "react";
-import { getUsernameFromToken } from "./getUserName";
+import { getUsernameFromToken, getUserRole } from "./getUserName";
 import { logout } from "./authLogout";
 import logo from "../images/logo.jpeg";
 import "./navbar.css";
 
 export default function Navbar() {
   const [username, setUsername] = useState(null);
-
+  const [userRole, setUserRole] = useState(null);
   useEffect(() => {
     const name = getUsernameFromToken();
     setUsername(name);
   }, []);
-
+  useEffect(() => {
+    const role = getUserRole();
+    setUserRole(role);
+  }, []);
   return (
     <>
       <div className="navbar">
@@ -38,7 +41,7 @@ export default function Navbar() {
               height="16"
               fill="currentColor"
               viewBox="0 0 16 16"
-              className="house-fill"
+              className="navbar-icon"
             >
               <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z" />
               <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z" />
@@ -60,7 +63,7 @@ export default function Navbar() {
               height="16"
               fill="currentColor"
               viewBox="0 0 16 16"
-              className="globe"
+              className="navbar-icon"
             >
               <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855q-.215.403-.395.872c.705.157 1.472.257 2.282.287zM4.249 3.539q.214-.577.481-1.078a7 7 0 0 1 .597-.933A7 7 0 0 0 3.051 3.05q.544.277 1.198.49zM3.509 7.5c.036-1.07.188-2.087.436-3.008a9 9 0 0 1-1.565-.667A6.96 6.96 0 0 0 1.018 7.5zm1.4-2.741a12.3 12.3 0 0 0-.4 2.741H7.5V5.091c-.91-.03-1.783-.145-2.591-.332M8.5 5.09V7.5h2.99a12.3 12.3 0 0 0-.399-2.741c-.808.187-1.681.301-2.591.332zM4.51 8.5c.035.987.176 1.914.399 2.741A13.6 13.6 0 0 1 7.5 10.91V8.5zm3.99 0v2.409c.91.03 1.783.145 2.591.332.223-.827.364-1.754.4-2.741zm-3.282 3.696q.18.469.395.872c.552 1.035 1.218 1.65 1.887 1.855V11.91c-.81.03-1.577.13-2.282.287zm.11 2.276a7 7 0 0 1-.598-.933 9 9 0 0 1-.481-1.079 8.4 8.4 0 0 0-1.198.49 7 7 0 0 0 2.276 1.522zm-1.383-2.964A13.4 13.4 0 0 1 3.508 8.5h-2.49a6.96 6.96 0 0 0 1.362 3.675c.47-.258.995-.482 1.565-.667m6.728 2.964a7 7 0 0 0 2.275-1.521 8.4 8.4 0 0 0-1.197-.49 9 9 0 0 1-.481 1.078 7 7 0 0 1-.597.933M8.5 11.909v3.014c.67-.204 1.335-.82 1.887-1.855q.216-.403.395-.872A12.6 12.6 0 0 0 8.5 11.91zm3.555-.401c.57.185 1.095.409 1.565.667A6.96 6.96 0 0 0 14.982 8.5h-2.49a13.4 13.4 0 0 1-.437 3.008M14.982 7.5a6.96 6.96 0 0 0-1.362-3.675c-.47.258-.995.482-1.565.667.248.92.4 1.938.437 3.008zM11.27 2.461q.266.502.482 1.078a8.4 8.4 0 0 0 1.196-.49 7 7 0 0 0-2.275-1.52c.218.283.418.597.597.932m-.488 1.343a8 8 0 0 0-.395-.872C9.835 1.897 9.17 1.282 8.5 1.077V4.09c.81-.03 1.577-.13 2.282-.287z" />
             </svg>
@@ -81,7 +84,7 @@ export default function Navbar() {
               height="16"
               fill="currentColor"
               viewBox="0 0 16 16"
-              className="chat-square-text"
+              className="navbar-icon"
             >
               <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
               <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6m0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5" />
@@ -102,7 +105,7 @@ export default function Navbar() {
               height="16"
               fill="currentColor"
               viewBox="0 0 16 16"
-              className="telephone-fill"
+              className="navbar-icon"
             >
               <path
                 fillRule="evenodd"
@@ -125,7 +128,7 @@ export default function Navbar() {
               height="16"
               fill="currentColor"
               viewBox="0 0 16 16"
-              className="person-circle"
+              className="navbar-icon"
             >
               <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
               <path
@@ -137,8 +140,9 @@ export default function Navbar() {
               <div className="fix-split-button">
                 <Dropdown as={ButtonGroup}>
                   <Button
-                    onClick={() => (window.location.href = "/user-profile")}
+                    onClick={() => (window.location.href = "/user-info")}
                     variant="success"
+                    className="donor-button"
                   >
                     {username}
                   </Button>
@@ -158,7 +162,7 @@ export default function Navbar() {
                           width="16"
                           height="16"
                           fill="currentColor"
-                          class="bi bi-person-fill"
+                          className="navbar-icon"
                           viewBox="0 0 16 16"
                         >
                           <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
@@ -181,7 +185,7 @@ export default function Navbar() {
                           width="16"
                           height="16"
                           fill="currentColor"
-                          class="bi bi-calendar-check-fill"
+                          className="navbar-icon"
                           viewBox="0 0 16 16"
                         >
                           <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2m-5.146-5.146-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708.708" />
