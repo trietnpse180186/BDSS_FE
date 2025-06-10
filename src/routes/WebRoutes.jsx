a2b4b58d211279aa176c15a699b810b6315fadcd
Thanh Nha <nhatltse183163@fpt.edu.vn>
2025-06-10 08:05:18 +0700

    Update WebRoutes.jsx

--- src/routes/WebRoutes.jsx
+++ src/routes/WebRoutes.jsx
@@ -9,6 +9,7 @@ import DonorRegister from "../pages/DonorRegister";
 import Contact from "../pages/Contact";
 import UserProfile from "../pages/UserProfile";
 import DonationSchedule from "../pages/DonationSchedule";
+import BloodRegister from "../pages/BloodRegister";
 // import BlogSection from "../components/BlogSection"; // Uncomment if you want to use BlogSection
 export default function WebRoutes() {
   return (
@@ -22,6 +23,7 @@ export default function WebRoutes() {
       <Route path="/contact" element={<Contact />} />
       <Route path="/user-profile" element={<UserProfile />} />
       <Route path="/schedule" element={<DonationSchedule />}></Route>
+      <Route path="/blood-registration" element={<BloodRegister />}></Route>
       {/* <Route path="/blog-section" element={<BlogSection />} /> Uncomment if you want to use BlogSection */}
     </Routes>
   );
